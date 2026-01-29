317f14ffd8ee83fbc124ac1d143da648b676b920
kalinkanya <copk-3@yandex.ru>
2026-01-29 13:10:21 +0300

    fix: correct audio paths for GitHub Pages

--- js/playList.js
+++ js/playList.js
@@ -1,22 +1,22 @@
 const playList = [
   {
     title: "Eco-technology",
-    src: "../assets/audio/eco-technology.mp3",
+    src: "assets/audio/eco-technology.mp3",
     duration: "02:02",
   },
   {
     title: "Fresh",
-    src: "../assets/audio/fresh.mp3",
+    src: "assets/audio/fresh.mp3",
     duration: "02:06",
   },
   {
     title: "Waterfall",
-    src: "../assets/audio/waterfall.mp3",
+    src: "assets/audio/waterfall.mp3",
     duration: "02:44",
   },
   {
     title: "Liza",
-    src: "../assets/audio/liza.mp3",
+    src: "assets/audio/liza.mp3",
     duration: "03:31",
   },
 ];
